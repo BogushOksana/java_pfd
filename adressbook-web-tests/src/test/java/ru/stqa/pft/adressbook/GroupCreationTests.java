889a4dca669e7d4fb9ae9ce4147feecd67e04271
ImOksana <okbogush@yandex.ru>
2019-03-15 01:02:05 +0500

    Рефакторинг первого теста для веб-приложения с использованием selenium

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.adressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,33 +14,57 @@ public class GroupCreationTests {
     vd = new FirefoxDriver();
     vd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     vd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     vd.findElement(By.name("user")).clear();
-    vd.findElement(By.name("user")).sendKeys("admin");
+    vd.findElement(By.name("user")).sendKeys(username);
     vd.findElement(By.name("pass")).clear();
-    vd.findElement(By.name("pass")).sendKeys("secret");
+    vd.findElement(By.name("pass")).sendKeys(password);
     vd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    vd.findElement(By.linkText("groups")).click();
-    vd.findElement(By.name("new")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test1", "test2", "test3"));
+    submitGroupCreation();
+    gotoGroupPage();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    vd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitGroupCreation() {
+    vd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     vd.findElement(By.name("group_name")).click();
     vd.findElement(By.name("group_name")).clear();
-    vd.findElement(By.name("group_name")).sendKeys("test1");
+    vd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     vd.findElement(By.name("group_header")).clear();
-    vd.findElement(By.name("group_header")).sendKeys("test2");
+    vd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     vd.findElement(By.name("group_footer")).clear();
-    vd.findElement(By.name("group_footer")).sendKeys("test3");
-    vd.findElement(By.name("submit")).click();
+    vd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    vd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
     vd.findElement(By.linkText("groups")).click();
-    vd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     vd.quit();
-    
+
   }
 
   private boolean isElementPresent(By by) {
