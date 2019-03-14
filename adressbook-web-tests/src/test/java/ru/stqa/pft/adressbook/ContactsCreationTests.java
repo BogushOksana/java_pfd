ebb77d6a914c2aa4bb09abd866d8079780a21a27
ImOksana <okbogush@yandex.ru>
2019-03-15 01:55:18 +0500

    Создание контакта. Перенос кода в метод инициальзации фикстуры

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/ContactsCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/ContactsCreationTests.java
@@ -6,51 +6,51 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ContactsCreationTests {
-  private WebDriver md;
+  private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    md = new FirefoxDriver();
-    md.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactsCreation() throws Exception {
-    md.get("http://localhost/addressbook/");
-    md.findElement(By.name("user")).clear();
-    md.findElement(By.name("user")).sendKeys("admin");
-    md.findElement(By.name("pass")).click();
-    md.findElement(By.name("pass")).clear();
-    md.findElement(By.name("pass")).sendKeys("secret");
-    md.findElement(By.xpath("//input[@value='Login']")).click();
-    md.findElement(By.linkText("add new")).click();
-    md.findElement(By.name("firstname")).click();
-    md.findElement(By.name("firstname")).clear();
-    md.findElement(By.name("firstname")).sendKeys("Иванов");
-    md.findElement(By.name("lastname")).click();
-    md.findElement(By.name("lastname")).clear();
-    md.findElement(By.name("lastname")).sendKeys("Иван");
-    md.findElement(By.name("nickname")).click();
-    md.findElement(By.name("nickname")).clear();
-    md.findElement(By.name("nickname")).sendKeys("Ваня");
-    md.findElement(By.name("mobile")).click();
-    md.findElement(By.name("mobile")).clear();
-    md.findElement(By.name("mobile")).sendKeys("123456789");
-    md.findElement(By.name("email")).click();
-    md.findElement(By.name("email")).clear();
-    md.findElement(By.name("email")).sendKeys("ivanov@mail");
-    md.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
-    md.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("Иванов");
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys("Иван");
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys("Ваня");
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys("123456789");
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("ivanov@mail");
+    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]")).click();
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    md.quit();
+    wd.quit();
   }
 
   private boolean isElementPresent(By by) {
     try {
-      md.findElement(by);
+      wd.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -59,7 +59,7 @@ public class ContactsCreationTests {
 
   private boolean isAlertPresent() {
     try {
-      md.switchTo().alert();
+      wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
