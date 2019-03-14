0d8db22db107807c092444bd53c7261ab3fbedf9
ImOksana <okbogush@yandex.ru>
2019-03-15 00:14:36 +0500

    Первый тест для веб-приложения, с использованием драйвера selenium

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
@@ -0,0 +1,62 @@
+package ru.stqa.pft.adressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupCreationTests {
+  private WebDriver vd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    vd = new FirefoxDriver();
+    vd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    vd.get("http://localhost/addressbook/");
+    vd.findElement(By.name("user")).clear();
+    vd.findElement(By.name("user")).sendKeys("admin");
+    vd.findElement(By.name("pass")).clear();
+    vd.findElement(By.name("pass")).sendKeys("secret");
+    vd.findElement(By.xpath("//input[@value='Login']")).click();
+    vd.findElement(By.linkText("groups")).click();
+    vd.findElement(By.name("new")).click();
+    vd.findElement(By.name("group_name")).click();
+    vd.findElement(By.name("group_name")).clear();
+    vd.findElement(By.name("group_name")).sendKeys("test1");
+    vd.findElement(By.name("group_header")).clear();
+    vd.findElement(By.name("group_header")).sendKeys("test2");
+    vd.findElement(By.name("group_footer")).clear();
+    vd.findElement(By.name("group_footer")).sendKeys("test3");
+    vd.findElement(By.name("submit")).click();
+    vd.findElement(By.linkText("groups")).click();
+    vd.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    vd.quit();
+    
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      vd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      vd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
