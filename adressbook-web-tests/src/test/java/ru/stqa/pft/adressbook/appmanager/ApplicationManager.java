ade135b4994a2fa56f512ee9de63f1ddfc8aa958
ImOksana <okbogush@yandex.ru>
2019-03-17 03:28:31 +0500

    Revert "Тест группы. Выделение специализированных классов-помощников"
    This reverts commit 01b4fdc150141bb29225ed8be14b3255e3d813eb.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -5,28 +5,53 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.adressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  WebDriver wd;
-
-  private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
+  private WebDriver wd;
 
   public void init() {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
     wd.get("http://localhost/addressbook/");
-    sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
-
+    login("admin", "secret");
   }
 
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
 
+  public void returnToGroupPage() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  public void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  public void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
 
   public void close() {
     wd.quit();
@@ -50,11 +75,11 @@ public class ApplicationManager {
     }
   }
 
-  public GroupHelper getGroupHelper() {
-    return groupHelper;
+  public void deleteSelectedGroups() {
+    wd.findElement(By.name("delete")).click();
   }
 
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
+  public void selectGroup() {
+    wd.findElement(By.name("selected[]")).click();
   }
 }
