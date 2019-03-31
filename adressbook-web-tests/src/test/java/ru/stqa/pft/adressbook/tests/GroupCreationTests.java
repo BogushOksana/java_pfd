971d6d6bd873de88efdbc92b99b3fc85700549a8
ImOksana <okbogush@yandex.ru>
2019-03-31 13:51:37 +0500

    4.1 и 4.2 Циклы: повторение похожих действий и коллекции: наборы элементов

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
@@ -7,7 +8,11 @@ public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
+    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before, +1);
   }
+
 }
