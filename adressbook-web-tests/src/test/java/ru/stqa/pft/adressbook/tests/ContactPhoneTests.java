21d149c6b6535d54ff9aaa44bd29a129880f3262
ImOksana <okbogush@yandex.ru>
2019-04-15 02:34:42 +0500

    5.11 Клеим строки. Метод обратных проверок (телефоны)

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactPhoneTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/ContactPhoneTests.java
@@ -1,8 +1,13 @@
 package ru.stqa.pft.adressbook.tests;
 
+import jdk.internal.dynalink.beans.StaticClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.ContactData;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,12 +29,18 @@ public class ContactPhoneTests extends TestBase {
     ContactData contact = app.contact().all().iterator().next(); // загрузка списка множества контактов, выбор контакта случайным образом
     ContactData contactInfoFromEditFrom = app.contact().infoFromEditForm(contact);
 
-    assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditFrom.getHomePhone())));
-    assertThat(contact.getMobile(), equalTo(cleaned(contactInfoFromEditFrom.getMobile())));
-    assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditFrom.getWorkPhone())));
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditFrom)));
+
   }
 
-  public String cleaned(String phone) {
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobile(), contact.getWorkPhone())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 
