0407056ed56490add3ccaa591a3281ad30ec5f11
ImOksana <okbogush@yandex.ru>
2019-04-21 00:41:37 +0500

    Лекция 6.3. Анализ опций командой строки при запуске генератора данных (Группы, контакты)

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/generators/GroupDataGenerator.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/generators/GroupDataGenerator.java
@@ -1,28 +1,50 @@
 package ru.stqa.pft.adressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.adressbook.model.GroupData;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.security.acl.Group;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupDataGenerator {
+public class GroupDataGenerator { //подключили библиотеку jcommander - не поддерживает работу с файлами напрямую
+
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
 
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]); //параметр передает количество групп (преобразованный в число)
-    File file = new File(args[1]);// параметр передает путь к файлу
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
 
+
+  }
+
+
+
+  private void run() throws IOException {
     //генерация данных
     List<GroupData> groups = generateGroups(count);
     //сохранение этих данных в файл
-    save(groups, file);
+    save(groups, new File(file));
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     //сохранение списка в файл
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file); // запись
@@ -32,7 +54,7 @@ public class GroupDataGenerator {
     writer.close(); //не забываем закрыть запись
   }
 
-private static List<GroupData> generateGroups(int count) {
+private List<GroupData> generateGroups(int count) {
   List<GroupData>groups = new ArrayList<GroupData>();
   for (int i = 0; i < count; i++) {
     groups.add(new GroupData().withName(String.format("test %s",i))
